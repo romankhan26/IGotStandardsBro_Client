5c70da9258e7c71bf167e1c71862894ea6a30205
roman khan <romankhan.26@outlook.com>
2025-02-01 16:12:18 +0500

    api url changes

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
 export const fetchData = async (data: Record<string, unknown>) => {
-    const response = await fetch("https://i-got-standards-bro-backend.onrender.com/api/v1/query/", {
+    const response = await fetch("https://i-got-standards-bro-demo.vercel.app/api/query", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
