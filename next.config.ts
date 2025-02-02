37d4cf6a7b6d8f0c7591d12e0a1a794573599811
roman khan <romankhan.26@outlook.com>
2025-02-02 15:58:05 +0500

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-        ],
-      },
-    ];
-  },
-};
-
-module.exports = nextConfig;
+import type { NextConfig } from "next";
+const nextConfig: NextConfig = {
+  /* config options here */}
+  export default nextConfig;
